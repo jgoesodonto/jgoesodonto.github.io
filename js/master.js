dafbb68d2dbcd00e906e4f4d63e1101aa0fc01ba
ralomach <ralomach@gmail.com>
2018-11-09 18:00:07 -0200

    init

--- js/master.js
+++ js/master.js
@@ -1,5 +1,7 @@
-document.querySelectorAll("a[href^='#']").forEach( function(anchor) {
-  anchor.addEventListener("click", function(e) {
+document.querySelectorAll("a[href^='#']").forEach(function(anchor) {
+  anchor.addEventListener(
+    "click",
+    function(e) {
       if (window.scrollTo) {
         var target = document.getElementById(
           this.getAttribute("href").substring(1)
@@ -9,4 +11,38 @@ document.querySelectorAll("a[href^='#']").forEach( function(anchor) {
       }
     }.bind(anchor)
   );
+});
+
+window.addEventListener("scroll", function() {
+  var yClinica = document.getElementById("clinicas").offsetTop;
+  var yEquipe = document.getElementById("equipe").offsetTop;
+  var yContato = document.getElementById("contato").offsetTop;
+  console.log(window.pageYOffset, yClinica, yEquipe, yContato);
+  if (window.pageYOffset < yClinica - 50) {
+    document.getElementById("home_icon").style.backgroundPositionY = '48px';
+    document.getElementById("place_icon").style.backgroundPositionY = '0';
+    document.getElementById("people_icon").style.backgroundPositionY = '0';
+    document.getElementById("phone_icon").style.backgroundPositionY = '0';
+  } else if (
+    window.pageYOffset > yClinica - 50 &&
+    window.pageYOffset < yEquipe - 50
+  ) {
+    document.getElementById("home_icon").style.backgroundPositionY = '0';
+    document.getElementById("place_icon").style.backgroundPositionY = '48px';
+    document.getElementById("people_icon").style.backgroundPositionY = '0';
+    document.getElementById("phone_icon").style.backgroundPositionY = '0';
+  } else if (
+    window.pageYOffset > yEquipe - 50 &&
+    window.pageYOffset < yContato - 50
+  ) {
+    document.getElementById("home_icon").style.backgroundPositionY = '0';
+    document.getElementById("place_icon").style.backgroundPositionY = '0';
+    document.getElementById("people_icon").style.backgroundPositionY = '48px';
+    document.getElementById("phone_icon").style.backgroundPositionY = '0';
+  } else if (window.pageYOffset > yContato - 50) {
+    document.getElementById("home_icon").style.backgroundPositionY = '0';
+    document.getElementById("place_icon").style.backgroundPositionY = '0';
+    document.getElementById("people_icon").style.backgroundPositionY = '0';
+    document.getElementById("phone_icon").style.backgroundPositionY = '48px';
+  }
 });
