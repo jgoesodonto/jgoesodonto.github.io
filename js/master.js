94ef18d56c02a1d39a79b8fa2d2773af70b2ace8
Rafael <ralomach@gmail.com>
2018-11-10 11:36:15 -0200

    init

--- js/master.js
+++ js/master.js
@@ -19,47 +19,47 @@ window.addEventListener("scroll", function() {
   var yContato = document.getElementById("contato").offsetTop;
   console.log(window.pageYOffset, yConsultorios, yEquipe, yContato);
   if (window.pageYOffset < yConsultorios - 50) {
-    document.getElementById("home_icon").style.backgroundPositionY = '36px';
-    document.getElementById("home_icon").style.backgroundPositionY = '36px';
-    document.getElementById("place_icon").style.backgroundPositionY = '0';
-    document.getElementById("people_icon").style.backgroundPositionY = '0';
-    document.getElementById("phone_icon").style.backgroundPositionY = '0';
-    document.getElementById("inicio_link").style.color = '#7ba3ce';
-    document.getElementById("consultorios_link").style.color = 'gray';
-    document.getElementById("equipe_link").style.color = 'gray';
-    document.getElementById("contato_link").style.color = 'gray';
+    document.getElementById("home_icon").style.backgroundPositionY = "36px";
+    document.getElementById("home_icon").style.backgroundPositionY = "36px";
+    document.getElementById("place_icon").style.backgroundPositionY = "0";
+    document.getElementById("people_icon").style.backgroundPositionY = "0";
+    document.getElementById("phone_icon").style.backgroundPositionY = "0";
+    document.getElementById("inicio_link").style.color = "#7ba3ce";
+    document.getElementById("consultorios_link").style.color = "gray";
+    document.getElementById("equipe_link").style.color = "gray";
+    document.getElementById("contato_link").style.color = "gray";
   } else if (
     window.pageYOffset > yConsultorios - 50 &&
     window.pageYOffset < yEquipe - 50
   ) {
-    document.getElementById("home_icon").style.backgroundPositionY = '0';
-    document.getElementById("place_icon").style.backgroundPositionY = '36px';
-    document.getElementById("people_icon").style.backgroundPositionY = '0';
-    document.getElementById("phone_icon").style.backgroundPositionY = '0';
-    document.getElementById("inicio_link").style.color = 'gray';
-    document.getElementById("consultorios_link").style.color = '#7ba3ce';
-    document.getElementById("equipe_link").style.color = 'gray';
-    document.getElementById("contato_link").style.color = 'gray';
+    document.getElementById("home_icon").style.backgroundPositionY = "0";
+    document.getElementById("place_icon").style.backgroundPositionY = "36px";
+    document.getElementById("people_icon").style.backgroundPositionY = "0";
+    document.getElementById("phone_icon").style.backgroundPositionY = "0";
+    document.getElementById("inicio_link").style.color = "gray";
+    document.getElementById("consultorios_link").style.color = "#7ba3ce";
+    document.getElementById("equipe_link").style.color = "gray";
+    document.getElementById("contato_link").style.color = "gray";
   } else if (
     window.pageYOffset > yEquipe - 50 &&
     window.pageYOffset < yContato - 50
   ) {
-    document.getElementById("home_icon").style.backgroundPositionY = '0';
-    document.getElementById("place_icon").style.backgroundPositionY = '0';
-    document.getElementById("people_icon").style.backgroundPositionY = '36px';
-    document.getElementById("phone_icon").style.backgroundPositionY = '0';
-    document.getElementById("inicio_link").style.color = 'gray';
-    document.getElementById("consultorios_link").style.color = 'gray';
-    document.getElementById("equipe_link").style.color = '#7ba3ce';
-    document.getElementById("contato_link").style.color = 'gray';
+    document.getElementById("home_icon").style.backgroundPositionY = "0";
+    document.getElementById("place_icon").style.backgroundPositionY = "0";
+    document.getElementById("people_icon").style.backgroundPositionY = "36px";
+    document.getElementById("phone_icon").style.backgroundPositionY = "0";
+    document.getElementById("inicio_link").style.color = "gray";
+    document.getElementById("consultorios_link").style.color = "gray";
+    document.getElementById("equipe_link").style.color = "#7ba3ce";
+    document.getElementById("contato_link").style.color = "gray";
   } else if (window.pageYOffset > yContato - 50) {
-    document.getElementById("home_icon").style.backgroundPositionY = '0';
-    document.getElementById("place_icon").style.backgroundPositionY = '0';
-    document.getElementById("people_icon").style.backgroundPositionY = '0';
-    document.getElementById("phone_icon").style.backgroundPositionY = '36px';
-    document.getElementById("inicio_link").style.color = 'gray';
-    document.getElementById("consultorios_link").style.color = 'gray';
-    document.getElementById("equipe_link").style.color = 'gray';
-    document.getElementById("contato_link").style.color = '#7ba3ce';
+    document.getElementById("home_icon").style.backgroundPositionY = "0";
+    document.getElementById("place_icon").style.backgroundPositionY = "0";
+    document.getElementById("people_icon").style.backgroundPositionY = "0";
+    document.getElementById("phone_icon").style.backgroundPositionY = "36px";
+    document.getElementById("inicio_link").style.color = "gray";
+    document.getElementById("consultorios_link").style.color = "gray";
+    document.getElementById("equipe_link").style.color = "gray";
+    document.getElementById("contato_link").style.color = "#7ba3ce";
   }
 });
